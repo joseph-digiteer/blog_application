871653e9299221f60aedf431bf0d24a791f33dba
Raphael Padua <raph@digiteer.digital>
2024-08-14 11:26:44 +0800

    added author to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
+  get 'my_posts', to: 'posts#my_posts'
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
