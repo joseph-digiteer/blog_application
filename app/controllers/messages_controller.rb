02400bf6e28e76843e1717d14e36eede320296aa
joseph-digiteer <jbeni.dev@gmail.com>
2024-08-20 14:59:20 +0800

    no need refresh to render what added prepend=top append=bottom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   # GET /messages or /messages.json
   def index
-    @messages = Message.all
+    @messages = Message.order(created_at: :desc)
   end
 
   # GET /messages/1 or /messages/1.json
@@ -22,17 +22,35 @@ class MessagesController < ApplicationController
   # POST /messages or /messages.json
   def create
     @message = Message.new(message_params)
-
+  
     respond_to do |format|
       if @message.save
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update("new_message", 
+                                partial: "messages/form", 
+                                locals: { message: Message.new }),
+            turbo_stream.prepend("messages", 
+                                partial: "messages/message", 
+                                locals: { message: @message})
+          ]
+        end
         format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
         format.json { render :show, status: :created, location: @message }
       else
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update("new_message", 
+                                partial: "messages/form", 
+                                locals: { message: @message })
+          ]
+        end
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
   end
+  
 
   # PATCH/PUT /messages/1 or /messages/1.json
   def update
